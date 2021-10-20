bf18d172debdf2cdb0c10e8a3dd5ffd611070431
yocosaka <sakayoco55@gmail.com>
2021-10-20 20:03:36 +0900

    Add Eventing class and cut on and trigger methods out of User and paste them in it

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,11 +7,7 @@ interface UserProps {
   [key: string]: string | number | undefined
 }
 
-type Callback = () => void
-
 export class User {
-  events: { [key: string]: Callback[]; } = {}
-
   constructor(
     private data: UserProps
   ) { }
@@ -25,22 +21,6 @@ export class User {
       ...this.data,
       ...update
     }
-  }
-
-  on (eventName: string, callback: Callback): void {
-    // register callback function on events
-    const handlers = this.events[eventName] || []
-    handlers.push(callback)
-    this.events[eventName] = handlers
-  }
-
-  trigger (eventName: string): void {
-    const handlers = this.events[eventName]
-    if (!handlers || handlers.length === 0) return
-
-    handlers.forEach((callback) => {
-      callback()
-    });
   }
 
   fetch (): void {
