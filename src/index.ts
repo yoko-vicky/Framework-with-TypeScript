2d04e16a9fd2d86364268ccf4aa9c0a85a7b4fbc
yocosaka <sakayoco55@gmail.com>
2021-10-20 18:14:52 +0900

    Add save method to User calss

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 })
-
-user.fetch()
-
-setTimeout(() => {
-  console.log(user)
-}, 4000);
+const user = new User({ name: 'Asahi', age: 41 })
+user.save()
--- src/models/User.ts
+++ src/models/User.ts
@@ -47,11 +47,16 @@ export class User {
     axios.get(`http://localhost:3000/users/${this.get('id')}`)
       .then((response: AxiosResponse): void => {
         console.log(response)
-        this.set(response.data)
+        this.set(response.data as UserProps)
     })
   }
 
   save (): void {
-    
+    const id = this.get('id')
+    if (id) {
+      axios.put(`http://localhost:3000/users/${id}`, this.data)
+    } else {
+      axios.post('http://localhost:3000/users', this.data)
+    }
   }
 }
