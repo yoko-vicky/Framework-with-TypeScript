0f61bb52fb33c8819c888d1c6cab7a940490ddf4
yocosaka <sakayoco55@gmail.com>
2021-10-21 11:01:02 +0900

    Include Eventing in User class to make it nested so you can call like user.events.on

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios'
+import { Eventing } from './Eventing'
 
 interface UserProps {
   id?: number
@@ -8,6 +9,8 @@ interface UserProps {
 }
 
 export class User {
+  public events: Eventing = new Eventing()
+
   constructor(
     private data: UserProps
   ) { }
