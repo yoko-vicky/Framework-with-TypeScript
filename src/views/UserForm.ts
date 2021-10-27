2a40edf55101465839324af8bbbc9cf2905566d2
yocosaka <sakayoco55@gmail.com>
2021-10-27 21:04:57 +0900

    Add bindModel method in UserForm to re-render the template with updated age every time updates

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,7 +4,15 @@ export class UserForm {
   constructor(
     public parent: Element,
     public model: User
-  ) { }
+  ) {
+    this.bindModel()
+  }
+
+  bindModel ():void {
+    this.model.on('change', () => {
+      this.render()
+    })
+  }
 
   eventsMap (): { [key: string]: ()=>void }{
     return {
@@ -43,6 +51,7 @@ export class UserForm {
     }
   }
   render (): void {
+    this.parent.innerHTML = ''
     const templateElement = document.createElement('template')
     templateElement.innerHTML = this.template()
 
