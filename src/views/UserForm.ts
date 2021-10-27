23212e4e671f925aafcaac747b8c8d1373109cec
yocosaka <sakayoco55@gmail.com>
2021-10-27 21:13:48 +0900

    Add onSetNameClick method to UserForm to handle input name value

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -16,7 +16,8 @@ export class UserForm {
 
   eventsMap (): { [key: string]: ()=>void }{
     return {
-      'click:.set-age': this.onSetAgeClick
+      'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick
     }
   }
 
@@ -25,14 +26,19 @@ export class UserForm {
     this.model.setRandomAge()
   }
 
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input')
+    this.model.set({name: input.value})
+  }
+
   template (): string {
     return `
     <div>
       <h1>User Form</h1>
       <div>User name: ${this.model.get('name')}</div>
       <div>User age: ${this.model.get('age')}</div>
-      <input />
-      <button>Click</button>
+      <input value="${this.model.get('name')}"/>
+      <button class="set-name">Change Name</button>
       <button class="set-age">Set Random Age</button>
     </div>
     `
