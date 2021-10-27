48c2110c5033083235b3888ec7703bace26ebc7e
yocosaka <sakayoco55@gmail.com>
2021-10-27 15:46:29 +0900

    Add deserialize property in fetch method of collection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const collection = new Collection('http://localhost:3000/users')
+const collection = new Collection<User, UserProps>(
+  'http://localhost:3000/users',
+  (json: UserProps) => User.buildUser(json)
+)
 
 collection.on('change', () => {
   console.log(collection)
--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -2,12 +2,13 @@ import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 import { User, UserProps } from './User'
 
-export class Collection {
-  models: User[] = []
+export class Collection<T, K> {
+  models: T[] = []
   events: Eventing = new Eventing()
 
   constructor(
-    public rootUrl: string
+    public rootUrl: string,
+    public deserialize: (json: K) => T
   ) { }
 
   get on () {
@@ -21,9 +22,9 @@ export class Collection {
   fetch ():void {
     axios.get(this.rootUrl)
       .then((response: AxiosResponse) => {
-        response.data.forEach((value: UserProps) => {
+        response.data.forEach((value: K) => {
           const user = User.buildUser(value)
-          this.models.push(user)
+          this.models.push(this.deserialize(value))
       })
       })
     this.trigger('change')
