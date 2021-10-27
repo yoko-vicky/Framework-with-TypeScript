8e711631a8447974c65f94805b19ecae728e5987
yocosaka <sakayoco55@gmail.com>
2021-10-27 15:35:13 +0900

    Add collection class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
-import { User } from './models/User';
+import { Collection } from "./models/Collection";
 
-const user = User.buildUser({ id: 1 });
+const collection = new Collection('http://localhost:3000/users')
 
-user.on('change', () => {
-  console.log(user);
+collection.on('change', () => {
+  console.log(collection)
 });
 
-user.fetch();
-console.log(user.isAdminUser())
+collection.fetch()
---
+++ src/models/Collection.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
+import { User, UserProps } from './User'
+
+export class Collection {
+  models: User[] = []
+  events: Eventing = new Eventing()
+
+  constructor(
+    public rootUrl: string
+  ) { }
+
+  get on () {
+    return this.events.on
+  }
+
+  get trigger () {
+    return this.events.trigger
+  }
+
+  fetch ():void {
+    axios.get(this.rootUrl)
+      .then((response: AxiosResponse) => {
+        response.data.forEach((value: UserProps) => {
+          const user = User.buildUser(value)
+          this.models.push(user)
+      })
+      })
+    this.trigger('change')
+  }
+}
