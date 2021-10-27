573b04331817f7d65deaab2acfd9b5d8cda30358
yocosaka <sakayoco55@gmail.com>
2021-10-27 15:49:52 +0900

    Add buildUserCollection class method in User class

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { Collection } from "./models/Collection";
 import { User, UserProps } from "./models/User";
 
-const collection = new Collection<User, UserProps>(
-  'http://localhost:3000/users',
-  (json: UserProps) => User.buildUser(json)
-)
-
+const collection = User.buildUserCollecton()
 collection.on('change', () => {
   console.log(collection)
 });
